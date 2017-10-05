530cdfaffadb2f82661512502a6bd74ee59eca10
Shukri Adams <shukri.adams@gmail.com>
2017-10-05 07:45:17 +0200

    remove redundant promise wrappers

--- utils/createGiveaway.js
+++ utils/createGiveaway.js
@@ -10,57 +10,50 @@ let settings = require('./settings').instance(),
     Store = require('./store');
 
 module.exports = async function create(message, client, start, duration, steamUrlInfo, code){
-    return new Promise(async function(resolve, reject){
-        try
-        {
-            let steamInfo = SteamInfo.instance();
-            let store = await Store.instance();
+    let steamInfo = SteamInfo.instance();
+    let store = await Store.instance();
 
-            // user needs to be admin or have giveaway permission
-            let hasPermission = await permissionHelper.isAdminOrHasRole(client, message.author, settings.values.giveawayRole);
-            if (!hasPermission){
-                message.author.send('You don\'t  have permission to create a giveaway');
-                return resolve(codes.MESSAGE_REJECTED_PERMISSION);
-            }
+    // user needs to be admin or have giveaway permission
+    let hasPermission = await permissionHelper.isAdminOrHasRole(client, message.author, settings.values.giveawayRole);
+    if (!hasPermission){
+        message.author.send('You don\'t  have permission to create a giveaway');
+        return codes.MESSAGE_REJECTED_PERMISSION;
+    }
 
-            let gameInfo = await steamInfo.get(steamUrlInfo.steamId, steamUrlInfo.steamUrl);
-            if (!gameInfo.success){
-                message.author.send(`Error : ${steamUrlInfo.steamId} is not a valid steam game id. \n` +
-                    'You can get the id from the game\'s store page on Steam. For example, store.steampowered.com/app/379720/DOOM/ is the URL for Doom, and the id is 379720');
-                return resolve(codes.MESSAGE_REJECTED_INVALIDSTEAMID);
-            }
+    let gameInfo = await steamInfo.get(steamUrlInfo.steamId, steamUrlInfo.steamUrl);
+    if (!gameInfo.success){
+        message.author.send(`Error : ${steamUrlInfo.steamId} is not a valid steam game id. \n` +
+            'You can get the id from the game\'s store page on Steam. For example, store.steampowered.com/app/379720/DOOM/ is the URL for Doom, and the id is 379720');
+        return codes.MESSAGE_REJECTED_INVALIDSTEAMID;
+    }
 
-            // try to get bracket - this can be null if no brackets exist
-            let bracket = bracketHelper.findBracketForPrice(gameInfo.price);
+    // try to get bracket - this can be null if no brackets exist
+    let bracket = bracketHelper.findBracketForPrice(gameInfo.price);
 
-            let giveaway = {
-                status : 'pending',
-                ownerId : message.author.id,
-                duration : duration.time,
-                durationMinutes : duration.minutes,
-                start :  start ? start.time : null,
-                startMinutes :  start ? start.minutes : null,
-                participants : [],
-                rejectedWinners : [],
-                code : code ? code : null,
-                created : new Date().getTime(),
-                lastUpdated : new Date().getTime(),
-                steamId : steamUrlInfo.steamId,
-                channelId : message.channel.id,
-                steamName: gameInfo.steamName,
-                price : gameInfo.price,
-                bracket : bracket ? bracketHelper.toString(bracket): null
-            };
+    let giveaway = {
+        status : 'pending',
+        ownerId : message.author.id,
+        duration : duration.time,
+        durationMinutes : duration.minutes,
+        start :  start ? start.time : null,
+        startMinutes :  start ? start.minutes : null,
+        participants : [],
+        rejectedWinners : [],
+        code : code ? code : null,
+        created : new Date().getTime(),
+        lastUpdated : new Date().getTime(),
+        steamId : steamUrlInfo.steamId,
+        channelId : message.channel.id,
+        steamName: gameInfo.steamName,
+        price : gameInfo.price,
+        bracket : bracket ? bracketHelper.toString(bracket): null
+    };
 
-            let queued = store.add(giveaway),
-                verb = start? 'Queued' : 'Starting';
+    let queued = store.add(giveaway),
+        verb = start? 'Queued' : 'Starting';
 
-            message.author.send(`${verb} giveaway id ${queued.id}, ${gameInfo.steamName}`);
-            infoLog.info(`${message.author.username} created giveaway id ${queued.id}, type ${verb}, ${gameInfo.steamName}.`);
-            resolve(codes.MESSAGE_ACCEPTED);
+    message.author.send(`${verb} giveaway id ${queued.id}, ${gameInfo.steamName}`);
+    infoLog.info(`${message.author.username} created giveaway id ${queued.id}, type ${verb}, ${gameInfo.steamName}.`);
+    return codes.MESSAGE_ACCEPTED;
 
-        } catch (ex){
-            reject(ex);
-        }
-    });
 };
